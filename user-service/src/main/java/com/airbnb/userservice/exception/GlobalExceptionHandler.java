06d0659e5d6d73c3ec561f4f63afd99ff937bf9c
Mustafa <mustafa.ercan392@gmail.com>
2024-01-22 21:51:02 +0200

    Implement API Gateway
    * Handle duplicate email error

--- user-service/src/main/java/com/airbnb/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/airbnb/userservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.airbnb.userservice.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +19,19 @@ public class GlobalExceptionHandler {
         body.put("timestamp", LocalDateTime.now());
         body.put("message", "User not found");
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDuplicateEmail(DataIntegrityViolationException ex) {
+        // Check if the exception is due to a duplicate key violation
+        if (ex.getMessage().contains("could not execute statement")) {
+            // Handle the duplicate key violation
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Email address already exists. Please use a different email.");
+        } else {
+            // Handle other data integrity violations if needed
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An unexpected error occurred.");
+        }
     }
 }
